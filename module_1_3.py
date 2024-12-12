866b4559db5f69b673004ae08e0d54ddaba827f2
Iginic <igi77777pankov@gmail.com>
2024-12-12 20:03:15 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,11 @@
+name="Igor"
+print(type(name))
+age=45
+print("Name:",name)
+print("Age:",age)
+New_age=age+2
+print("NEW Age",New_age)
+is_student=True
+is_student=+is_student
+print("Is Student:",is_student>0)
+
