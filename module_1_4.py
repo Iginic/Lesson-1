c86c3d1f035ac87c6086c3634352caaef0fd0233
Iginic <igi77777pankov@gmail.com>
2024-12-15 13:53:34 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,6 @@
+
+DZ_count=12
+time_count=1.5
+course='PYTHON'
+time_sr=time_count/DZ_count
+print('Курс:',course,'Всего задач:',DZ_count,'Затрачено часов:',time_count,'Среднее время выполнения:',time_sr,'часа')
