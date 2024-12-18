c7257cf314983559f3d1ed2a867533944cacbc7c
Iginic <igi77777pankov@gmail.com>
2024-12-18 21:33:12 +0300

    Update module_1_7.py

--- module_1_7.py
+++ module_1_7.py
@@ -1,10 +1,14 @@
 immutable_var = 1,2, 'a', 'b'
 print('Immutable tuple',immutable_var)
+print(type(immutable_var))
 #immutable_var[3] = 55  // кортежи не поддерживают изменение элементов
 #но можно изменять элементы внутри СПИСКА в кортеже
-mutable_list = 0,[1,2, 'a','b', 'XXX']
-mutable_list[1][4] = 'Modifed' #// замена в списке XXX на Modifid
-print('Mutable LiST',mutable_list[1:])
+mutable_list = [1, 2, 'a', 'b', 'XXX']
+print(type(mutable_list))
+print('Mutable_list',mutable_list)
+mutable_list.remove('XXX') #// замена в списке XXX на Modifid
+mutable_list.insert(5,'Modifid')
+print('Mutable LiST',mutable_list)
 
 
 
